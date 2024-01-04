4ee3582fa4c43665495ab5d388a8859967a436db
trunglam7 <trunglam107@gmail.com>
2024-01-03 17:23:29 -0800

    created dynamic component no ssr

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,11 @@
 
-import GeneticAlgorithmSimulation from './components/GeneticAlgorithmSimulation'
 import styles from './page.module.css'
+import dynamic from 'next/dynamic';
+
+const GeneticAlgorithmSimulation = dynamic(
+  () => import('./components/GeneticAlgorithmSimulation'),
+  { ssr: false }
+)
 
 export default function Home() {
   return (
